51c02fabd8e650a9077abd88fe899344300e8f06
Matteo Barbieri <matteo.barbieri@gmail.com>
2020-02-27 14:52:09 +0100

    Remove script for data download

--- download_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-wget https://storage.googleapis.com/peltarion-ml-assignment/HAM10000/HAM10000_images_part_1.zip
-wget https://storage.googleapis.com/peltarion-ml-assignment/HAM10000/HAM10000_images_part_2.zip
-wget https://storage.googleapis.com/peltarion-ml-assignment/HAM10000/HAM10000_metadata.csv
-unzip -qq HAM10000_images_part_1.zip -d data
-unzip -qq HAM10000_images_part_2.zip -d data
-mv HAM10000_metadata.csv data/
