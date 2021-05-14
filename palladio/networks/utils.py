2f07cbb09363d76b264690c15a5aa323cf5fd773
Matteo Barbieri <matteo.a.barbieri@gmail.com>
2021-05-14 13:30:15 +0000

    Add densenet121 to the list of available networks

--- palladio/networks/utils.py
+++ palladio/networks/utils.py
@@ -4,6 +4,9 @@ from torchvision.models.alexnet import alexnet  # noqa
 from torchvision.models.resnet import (  # noqa
     resnet18, resnet34, resnet50, resnet101, resnet152)
 
+from torchvision.models.densenet import (  # noqa
+    densenet121)
+
 from efficientnet_pytorch import EfficientNet
 
 import torch.nn as nn
@@ -40,6 +43,22 @@ def get_network(network_name, num_classes, use_pretrained, n_input_channels=3):
             net = EfficientNet.from_name(
                 'efficientnet-b7', in_channels=n_input_channels,
                 num_classes=num_classes)
+
+    if network_name.startswith("densenet"):
+        name_class_map = {
+            'densenet121': densenet121,
+        }
+
+        torchvision_class = name_class_map[network_name]
+
+        net = torchvision_class(pretrained=use_pretrained)
+        net.classifier = nn.Linear(net.classifier.in_features, num_classes)
+
+        # If the number of input channels is != 3, adapt network
+        if n_input_channels != 3:
+            net.features.conv0 = nn.Conv2d(
+                n_input_channels, 64, kernel_size=7, stride=2, padding=3,
+                bias=False)
 
     if network_name.startswith("resnet"):
         name_class_map = {
